d4896405e32f5673886a0632b41ddac11b01a12d
xh270731784 <270731784@qq.com>
2019-09-05 13:47:10 +0800

    add

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,23 @@
+import java.math.BigDecimal;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("com-sdj");
+		//zx
+		BigDecimal a = new BigDecimal("83000");
+		//ali+pufa
+		BigDecimal b= new BigDecimal("27000");
+		//laosij
+		BigDecimal c= new BigDecimal("20000");
+		//zhh
+		BigDecimal d= new BigDecimal("10000");
+		
+		BigDecimal f = a.add(b);
+		
+		BigDecimal e = c.add(d);
+		System.out.println(f.add(e));
 	}
 
 }
